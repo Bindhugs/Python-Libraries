e3819eebc4491b94605f2f182bb379f8f489cf57
Bindhugs <b6243451@gmail.com>
2026-04-04 19:00:56 +0530

    numpy.py

--- NumPy/Examples2.py
+++ NumPy/Examples2.py
@@ -6,8 +6,6 @@ array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
 array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
 print(array1*array2)
 
-
-
 array = np.array([1,2,3,4,5,6,7,8,9,10])
 print(np.sum(array)) #sum of all elements
 print(np.mean(array)) #mean 
